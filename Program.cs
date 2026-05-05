3d6e26257ec7ed2242cdc6b4108407c43df3e23c
is1omzhon <islomjonsharipov003@gmail.com>
2026-05-05 18:15:52 +0300

    Add gitignor

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine();
+﻿Console.WriteLine("Bu dastur amallarni bajaradi(1+4-2+2+1) ko'rinishida. \nAgar dasturdan foydalanmoqchi bulsangiz amalni kiriting: ");
+
+string misol = Console.ReadLine();
+int natija = misol[0] - '0';
+int i = 1;
+
+while (i < misol.Length)
+{
+    char amal = misol[i];
+    int num = misol[i + 1] - '0';
+    
+    switch (amal)
+    {
+        case '+':
+            natija = natija + num;
+            break;
+        case '-':
+            natija = natija - num;
+            break;
+        case '*':
+            natija = natija * num;
+            break;
+        case '/':
+            if (num != 0)
+            {
+                natija = natija / num;
+            }
+            else
+            {
+                Console.WriteLine("Xato: Nolga bo'lish mumkin emas!");
+                return;
+            }
+            break;
+        default:
+            Console.WriteLine($"Xato: '{amal}' amali mavjud emas!");
+            return;
+    }
+    
+    i += 2;
+}
+
+Console.WriteLine("Natija: " + natija);
